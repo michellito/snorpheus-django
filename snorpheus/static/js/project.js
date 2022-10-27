5722670c911edf5a4b55257dd55317bb225c2fef
Michelle Yung <myung@email.arizona.edu>
2022-10-27 12:08:20 -0700

    add basic d3 example

--- snorpheus/static/js/project.js
+++ snorpheus/static/js/project.js
@@ -1,1 +1,9 @@
 /* Project specific Javascript goes here. */
+
+import * as d3 from "https://cdn.skypack.dev/d3@7"
+
+const div = d3.selectAll("div")
+
+d3
+  .select(".target")  // select the elements that have the class 'target'
+  .style("stroke-width", 8) // change their style: stroke width is not equal to 8 pixels
