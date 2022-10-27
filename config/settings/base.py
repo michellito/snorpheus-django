d6d45fa33f0d5a9c074a6d7f7fa72c4485d301d4
Michelle Yung <myung@email.arizona.edu>
2022-10-27 11:51:42 -0700

    add htmx

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
     "rest_framework.authtoken",
     "corsheaders",
     "drf_spectacular",
+    "django_htmx",
 ]
 
 LOCAL_APPS = [
@@ -137,6 +138,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 # STATIC
