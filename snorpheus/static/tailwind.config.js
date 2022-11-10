7442d177067da4026ef7b684d1a01c8167b57f7d
Michelle Yung <myung@email.arizona.edu>
2022-11-10 11:08:00 -0700

    move tailwind to static folder

--- snorpheus/static/tailwind.config.js
+++ snorpheus/static/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./static/**/*.{html,js}", "./templates/**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "../templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
