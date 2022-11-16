4c02c5a0e248e6e3aab67d43b86e6195dde22c88
Michelle Yung <myung@email.arizona.edu>
2022-11-15 22:28:13 -0700

    setup for d3

--- snorpheus/static/js/project.js
+++ snorpheus/static/js/project.js
@@ -2,8 +2,29 @@
 
 import * as d3 from "https://cdn.skypack.dev/d3@7"
 
-const div = d3.selectAll("div")
+console.log('hiiii')
 
-d3
-  .select(".target")  // select the elements that have the class 'target'
-  .style("stroke-width", 8) // change their style: stroke width is not equal to 8 pixels
+let width = 1100;
+let height = window.innerWidth;
+let paddingLeft = 120;
+let paddingRight = 75;
+
+// set up svg canvas
+let svg = d3.select("#main")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+
+let timelineGroup = d3.select("#main").select("svg")
+  .append("g")
+  .attr("id", "timeline")
+  .attr("transform", `translate (${0}, ${20})`);
+
+timelineGroup.append("rect")
+  .attr("x", paddingLeft)
+  .attr("y", 0)
+  .attr("height", 30)
+  .attr("width", width - paddingLeft - paddingRight)
+  .style("stroke", d3.rgb(169,169,169))
+  .style("stroke-width", "2")
+  .style("fill", d3.rgb(211,211,211))
