b0473aab4c2b14f3afc373e4e6b0dab5376b1638
Michelle Yung <myung@email.arizona.edu>
2022-11-18 00:25:22 -0700

    start axes for time/position

--- snorpheus/static/js/home.js
+++ snorpheus/static/js/home.js
@@ -1,53 +1,9 @@
-// ------------- State Functions ----------------- //
-
-function toggleSidebar() {
-  let self = this;
-  self.sidebarOpen = !self.sidebarOpen;
-}
-
-function getPatientPeriods(clientId) {
-  let self = this;
-  axios.get('data/patients/' + clientId)
-    .then(function (response) {
-      // handle success
-      console.log(response)
-      self.patientPeriods = response.data;
-
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-}
-
-function watchPatientId(value) {
-  let self = this;
-  if (!value.length) {
-    self.patientPeriods = [];
-  }
-}
-
-function state() {
-
-  return {
-    // vars
-    sidebarOpen: true,
-    patientId: "",
-    patientPeriods: [],
-    positionEvents: [],
-    // functions
-    watchPatientId,
-    toggleSidebar,
-    getPatientPeriods,
-  }
-}
-
-// ------------- State Functions ----------------- //
 
 let width = 1100;
 let height = window.innerWidth;
 let paddingLeft = 120;
 let paddingRight = 75;
+let sleepSessions;
 
 // set up svg canvas
 let svg = d3.select("#main")
@@ -68,3 +24,217 @@ timelineGroup.append("rect")
   .style("stroke", d3.rgb(169,169,169))
   .style("stroke-width", "2")
   .style("fill", d3.rgb(211,211,211))
+
+// ------------- State Functions ----------------- //
+
+function toggleSidebar() {
+  let self = this;
+  self.sidebarOpen = !self.sidebarOpen;
+}
+
+function getPatientPeriods(clientId) {
+  let self = this;
+  axios.get('data/patients/' + clientId)
+    .then(function (response) {
+      // handle success
+      console.log(response)
+      self.patientPeriods = response.data.collection_periods;
+      self.patientName = response.data.patient_name;
+
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+}
+
+function getSessionPosition(sessionId) {
+  let self = this;
+  axios.get('data/sessions/' + sessionId + '/position')
+    .then(function (response) {
+      // handle success
+      console.log(response)
+      sleepSessions = [response.data];
+      console.log(sleepSessions)
+      setScales(sleepSessions)
+      setupCharts()
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+}
+
+function watchPatientId(value) {
+  let self = this;
+  if (!value.length) {
+    self.patientPeriods = [];
+  }
+}
+
+function setupCharts() {
+  let self = this;
+
+  svg.selectAll(".chart")
+    .data(sleepSessions, d => d.id)
+    .join(
+      function(enter) {
+        let group = enter.append("g")
+          .attr("class", "chart")
+          .attr("transform", function(d, i) {
+            return `translate(${0}, ${100 + (i * 80)})`
+          })
+          .each(function(d) {
+            // drawData(d3.select(this), d)
+            drawAxes(d3.select(this), d)
+          })
+      },
+      // function(update) {
+      //   let group = update
+      //     .transition()
+      //     .duration(1000)
+      //     .attr('transform', (d,i) => `translate(${0}, ${100 + (i * 80)})`)
+      //     .each(function(d) {
+      //       updateData(d3.select(this), d)
+      //       updateAxes(d3.select(this), d)
+      //     })
+      // },
+      // function(exit) {
+      //   exit.remove();
+      // }
+    );
+
+}
+
+function getExtents(sleepSessions) {
+
+  function getExtent(extents) {
+    let min = extents[0][0]
+    let max = extents[0][1]
+
+    extents.forEach(extent => {
+      if (extent[0] < min) {
+        min = extent[0];
+      }
+
+      if (extent[1] > max) {
+        max = extent[1]
+      }
+    });
+
+    return [min, max];
+  }
+
+  let timeExtents = [];
+
+  console.log(sleepSessions)
+
+  sleepSessions.forEach( session => {
+    console.log(session)
+    timeExtents.push(
+      d3.extent(session.position_data.map(
+        function(d) {
+          return d.timestamp;
+        }
+      ))
+    )
+  })
+
+  timeRange = getExtent(timeExtents);
+
+  return {
+    time: timeRange,
+  }
+}
+
+function setScales(data) {
+
+  console.log(data)
+
+  let extents = getExtents(data);
+  console.log(extents)
+  let timeDomain = extents.time;
+
+  console.log(extents)
+
+  // time scales
+  timeScale = d3.scaleTime()
+    .domain(timeDomain)
+    .range([paddingLeft, width - paddingRight])
+
+  timeAxis = d3.axisBottom()
+    .scale(timeScale);
+
+  // daily steps scale
+  positionScale = d3.scaleOrdinal()
+    .domain(['left', 'supine', 'right', 'other'])
+    .range([100, 0]);
+
+  positionAxis = d3.axisLeft()
+    .scale(positionScale)
+    .ticks(3);
+
+  positionColorScale = d3.scaleOrdinal(
+    d3.interpolatePurples)
+    .domain(['left', 'supine', 'right', 'other'])
+}
+
+function drawAxes(group, d) {
+
+  let axis, label;
+
+  axis = positionAxis;
+  className = 'positionAxis';
+  label = 'Position';
+
+  group.append("text")
+    .text(function(d) {
+      return d.id
+    })
+    .attr("transform", `translate(${0}, ${15})`)
+    .attr("class", "participant-label")
+
+  group.append("text")
+    .text(function(d) {
+      return label
+    })
+    .attr("transform", `translate(${0}, ${38})`)
+    .attr("class", "attribute-label")
+
+  // group.append("text")
+  //   .text(function(d) {
+  //     return units
+  //   })
+  //   .attr("transform", `translate(${0}, ${53})`)
+  //   .attr("class", "unit-label")
+
+
+  group.append("g")
+    .attr("class", "timeAxis")
+    .call(timeAxis)
+    .attr("transform", `translate(${0}, ${50})`)
+
+  group.append("g")
+    .attr("class", className)
+    .call(axis)
+    .attr("transform", `translate(${paddingLeft}, ${0})`)
+}
+
+function state() {
+
+  return {
+    // vars
+    sidebarOpen: true,
+    patientId: "",
+    patientName: "",
+    patientPeriods: [],
+    sleepSessions: [],
+    // functions
+    watchPatientId,
+    toggleSidebar,
+    getPatientPeriods,
+    getSessionPosition,
+  }
+}
+
+// ------------- State Functions ----------------- //
